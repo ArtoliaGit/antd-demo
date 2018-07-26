0594160b884f0591556d9820f8d4d827295bef91
ArtoliaGit <dxf1994@gmail.com>
2018-07-26 12:36:05 +0800

    修改目录结构，配置代理

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import Vue from 'vue';
+import Antd from 'vue-antd-ui';
+import 'vue-antd-ui/dist/antd.css';
 import App from './App.vue';
-import router from './router';
-import store from './store';
+import router from './router/router';
+import store from './store/store';
 
 Vue.config.productionTip = false;
+
+Vue.use(Antd);
 
 new Vue({
   router,
