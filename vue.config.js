f464b77c3e678d44dfabca8bd22096dc89bd293e
ArtoliaGit <dxf1994@gmail.com>
2018-07-26 09:29:28 +0800

    Initial commit

---
+++ vue.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  baseUrl: undefined,
+  outputDir: undefined,
+  assetsDir: 'src/assets',
+  runtimeCompiler: undefined,
+  productionSourceMap: undefined,
+  parallel: undefined,
+
+  css: {
+    modules: true,
+    sourceMap: true,
+  },
+
+  lintOnSave: undefined,
+};
